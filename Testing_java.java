8f0984f54ea30651c8725e832fbb78ff4b5df6f9
krishnateja-gopidinni <gopidinni.krishnateja@gmail.com>
2017-04-13 17:05:33 +0530

    changes done by krishana_feature_branch

--- Testing_java.java
+++ Testing_java.java
@@ -13,15 +13,8 @@ public class Testing_java{
 		System.out.println("sum is "+sum_res);
 		System.out.println("KRISHJA TE JBSBHSBDHJS");
 		System.out.println("Sai Raj JBSBHSBDHJS");
-		System.out.println("This is modified by krishna");
-        		System.out.println("This is modified due to sai");
-        		System.out.println("Hi KT i too know how to update in Git using push command");
-        		System.out.println("dhbdhvbfhx");
-        		System.out.println("Modifed--------------krishna----------");
-        =======
-        		System.out.println("KRISHJA TE JBSBHSBDHJS");
-        		System.out.println("Sai Raj JBSBHSBDHJS");
-        		System.out.println("i m updating this file by sairaj");
+		System.out.println("Sandeep");
+
 
 
 	}
