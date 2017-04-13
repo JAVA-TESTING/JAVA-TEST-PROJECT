4fdef947520eb5782b852a192eca9c27681e9b06
SAIRAJ-KOTAGIRI <saikotagiri2013@gmail.com>
2017-04-13 17:02:34 +0530

    by sairaj

--- Testing_java.java
+++ Testing_java.java
@@ -11,18 +11,17 @@ public class Testing_java{
 		Sum_class s=new Sum_class();
 		int sum_res = s.sum_method(a,b);
 		System.out.println("sum is "+sum_res);
-<<<<<<< HEAD
-		System.out.println("This is modified by krishna");
-		System.out.println("This is modified due to sai");
-		System.out.println("Hi KT i too know how to update in Git using push command");
-		System.out.println("dhbdhvbfhx");
-		System.out.println("Modifed--------------krishna----------");
-=======
 		System.out.println("KRISHJA TE JBSBHSBDHJS");
 		System.out.println("Sai Raj JBSBHSBDHJS");
-		System.out.println("Sandeep");
+		System.out.println("This is modified by krishna");
+        		System.out.println("This is modified due to sai");
+        		System.out.println("Hi KT i too know how to update in Git using push command");
+        		System.out.println("dhbdhvbfhx");
+        		System.out.println("Modifed--------------krishna----------");
+        =======
+        		System.out.println("KRISHJA TE JBSBHSBDHJS");
+        		System.out.println("Sai Raj JBSBHSBDHJS");
+        		System.out.println("i m updating this file by sairaj");
 
-
->>>>>>> changes done by krishana_feature_branch
 	}
 }
