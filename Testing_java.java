3c083814cc8909dc7bb95271cc2e1e78f987d0ff
krishnateja-gopidinni <gopidinni.krishnateja@gmail.com>
2017-04-13 17:26:44 +0530

    changes done by krishana_feature_branch

--- Testing_java.java
+++ Testing_java.java
@@ -17,5 +17,7 @@ public class Testing_java{
 		System.out.println("dhbdhvbfhx");
 		System.out.println("BY Kotagiri sairaj");
 		System.out.println("Arey KT we will get conflicts if we do modification on one file at a time");
+		System.out.println("This is modified due to sai");
+                		System.out.println("Hi KT i too know how to update in Git using push command")
 	}
 }
