6d98b63b1e8e0653a9e1c5e0b00b5f5dd6335330
SAI-KOTAGIRI <sairajkotagiri.k@gmail.com>
2017-04-13 12:37:56 +0530

    JAvA PROGRAM BY SAIRAJ

---
+++ Testing_java.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Testing_java{
+	
+	public static void main(String []args){
+		Scanner sc=new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		Sum_class s=new Sum_class();
+		int sum_res = s.sum_method(a,b);
+		System.out.println("sum is "+sum_res);
+	}
+}
