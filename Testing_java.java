be89c1bc1fb9ac247f1b704219121290710c47d5
SAIRAJ-KOTAGIRI <saikotagiri2013@gmail.com>
2017-04-14 11:33:09 +0530

    by sairaj

--- Testing_java.java
+++ Testing_java.java
@@ -12,15 +12,15 @@ public class Testing_java{
 		int sum_res = s.sum_method(a,b);
 		System.out.println("sum is "+sum_res);
 		System.out.println("This is modified by krishna");
-        		System.out.println("This is modified due to sai");
-        		System.out.println("Hi KT i too know how to update in Git using push command");
-        		System.out.println("dhbdhvbfhx");
-        		System.out.println("BY Kotagiri sairaj");
-        		System.out.println("Arey KT we will get conflicts if we dso modification on one file at a time");
-        		System.out.println("This is modified due to sai");
-                System.out.println("Hi KT i too know how to update in Git using push command");
-                System.out.println("hey i'm dng for the next time");
-                System.out.println("new update from sairaj");
+        System.out.println("This is modified due to sai");
+        System.out.println("Hi KT i too know how to update in Git using push command");
+        System.out.println("dhbdhvbfhx");
+        System.out.println("BY Kotagiri sairaj");
+        System.out.println("Arey KT we will get conflicts if we dso modification on one file at a time");
+        System.out.println("This is modified due to sai");
+        System.out.println("Hi KT i too know how to update in Git using push command");
+        System.out.println("hey i'm dng for the next time");
+        System.out.println("new update from sairaj");
 
 
 	}
